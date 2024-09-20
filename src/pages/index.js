eab42f594c61345f874df125789bd7bc39cb3ec3
Richjerk <puseletso55@gmail.com>
2024-09-20 10:32:56 +0200

    Your commit message

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Township Business Directory!</h1>
+      <p>Get started by exploring local businesses.</p>
+    </div>
+  );
+}
